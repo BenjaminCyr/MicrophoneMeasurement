0fc00c32d3c6d4479db5e0f933ccb4d0a579524e
Benjamin Cyr <bencyr39@gmail.com>
2022-03-09 14:32:25 -0800

    Added lots of recent experiments

--- Inc/set_fgen.m
+++ Inc/set_fgen.m
@@ -5,7 +5,7 @@ function [] = set_fgen(deviceObj, frequency, amplitude, offset)
 % Configure property value(s).
 %set(deviceObj.Output(1), 'State', 'off');
 set(deviceObj.Frequency(1), 'Frequency', frequency)
-set(deviceObj.Voltage(1), 'Amplitude', amplitude)
+set(deviceObj.Voltage(1), 'Amplitude', 2*amplitude)
 set(deviceObj.Voltage(1), 'Offset', offset )
 pause(0.01);
 end
